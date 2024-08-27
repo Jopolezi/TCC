f81588af4f65074a62f6ecb5b8b0e121220e8d4b
João Polezi <joao.francisco12123@gmail.com>
2024-08-27 11:21:26 -0300

    Atualização 3.1
    Adicionado ScrollReveal;
    Adicionado Download Indisponível (Mobile);
    Terminando card do section.projeto;
    Iniciado card do section.feedback;
    
    em processo:
    Finalização da responsividade;
    Menu Hamburger;

--- src/js/script.js
+++ src/js/script.js
@@ -13,27 +13,72 @@ document.addEventListener("scroll", ()=>{
 
  // Função para detectar se o usuário está em um dispositivo móvel
  function isMobileDevice() {
-    return /Mobi|Android|iPhone|iPad/i.test(navigator.userAgent);
-  }
+  return /Mobi|Android|iPhone|iPad/i.test(navigator.userAgent);
+}
 
-  document.addEventListener("DOMContentLoaded", function() {
-    var downloadLink = document.getElementById('download-link');
-    var notAvailableMessage = document.getElementById('not-available-message');
+document.addEventListener("DOMContentLoaded", function() {
+  var downloadLink = document.getElementById('download-link');
+  var notAvailableMessage = document.getElementById('not-available-message');
 
-    if (isMobileDevice()) {
+  // Verifica se o dispositivo é móvel
+  if (isMobileDevice()) {
+      // Esconde o link de download e exibe a mensagem de não disponível
       downloadLink.style.display = 'none';
       notAvailableMessage.style.display = 'block';
-    } else {
+  } else {
+      // Exibe o link de download e esconde a mensagem de não disponível
       downloadLink.style.display = 'block';
       notAvailableMessage.style.display = 'none';
-    }
-  });
+  }
+});
 
-  document.addEventListener("DOMContentLoaded", function() {
-    if (!isMobileDevice()) {
+document.addEventListener("DOMContentLoaded", function() {
+  // Adiciona uma classe ao corpo caso não seja um dispositivo móvel
+  if (!isMobileDevice()) {
       document.body.classList.add('desktop');
-    }
-  });
+  }
+});
+
+
+// Scroll Reveal
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal('.conteudo', { 
+  duration: 2000,
+  origin: 'left',
+  distance: '10%'
+});
+
+ScrollReveal().reveal('#reveal-1', {
+  duration: 2000,
+  origin: 'bottom',
+  distance: '10%'
+})
+
+ScrollReveal().reveal('#reveal-2', {
+  duration: 2000,
+  origin: 'bottom',
+  distance: '10%'
+})
+
+ScrollReveal().reveal('#card', {
+  duration: 2000,
+  origin: 'bottom',
+  distance: '10%'
+})
+
+ScrollReveal().reveal('#card-feedback', {
+  duration: 2000,
+  origin: 'bottom',
+  distance: '10%'
+})
+
+
+
+
+
+
+
 
   // Hamburger
   
